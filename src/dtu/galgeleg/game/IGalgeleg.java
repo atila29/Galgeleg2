5b74abb2c23f8850a25d8e845dc031eb75d7a9a6
champen <champen@champen-Pc>
2016-02-13 22:10:10 +0100

    RMI interface lavet

--- src/dtu/galgeleg/game/IGalgeleg.java
+++ src/dtu/galgeleg/game/IGalgeleg.java
@@ -12,6 +12,11 @@ import java.util.ArrayList;
  * @author herErEtNavn
  */
 public interface IGalgeleg extends java.rmi.Remote{
+    
+    static final int STATUS_VUNDET = 2;
+    static final int STATUS_TABT = 4;
+    static final int STATUS_IGANG = 8;
+    
     String getVisibleWord() throws java.rmi.RemoteException;
     int getStatus() throws java.rmi.RemoteException;
     int guessLetter(char letter) throws java.rmi.RemoteException;
---
+++ src/dtu/galgeleg/game/server/GalgelegImpl.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dtu.galgeleg.game.server;
+
+import dtu.galgeleg.game.IGalgeleg;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author kongerne
+ */
+public class GalgelegImpl extends UnicastRemoteObject implements IGalgeleg{
+
+    private Galgelogik logik;
+    
+    public GalgelegImpl() throws java.rmi.RemoteException {
+        logik = new Galgelogik();
+    }
+    
+    @Override
+    public String getVisibleWord() throws RemoteException {
+        return logik.getSynligtOrd();
+    }
+
+    @Override
+    public int getStatus() throws RemoteException {
+        
+    }
+
+    @Override
+    public int guessLetter(char letter) throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<String> getUsedLetters() throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void resetGame() throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
